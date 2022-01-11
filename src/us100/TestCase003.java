42fbc0b68b0acb077713d799d031a0b47afcadbd
YanchunZhou <tifa1981@gmail.com>
2022-01-11 12:30:41 -0500

    git rm --cached git_practice2.iml

---
+++ src/us100/TestCase003.java
@@ -0,0 +1,9 @@
+package us100;
+
+public class TestCase003 {
+
+    //git rm --cached git_practice2.iml
+
+
+
+}
---
+++ src/us100/TestCase04.java
@@ -0,0 +1,12 @@
+package us100;
+
+public class TestCase04 {
+    public static void main(String[] args) {
+        System.out.println("git rm --cached git_practice2.iml");
+
+
+
+
+
+    }
+}
